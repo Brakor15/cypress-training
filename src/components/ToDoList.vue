637a3a3208e9adb02954316274663f0b1bc1edd8
Yann Pascoet <ypascoet@proton.me>
2025-10-25 14:28:52 +0200

    Ajout d'une composant ToDoList

---
+++ src/components/ToDoList.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import type { Task } from "@/models/Task";
+
+interface Props {
+  title: string
+}
+
+const id = ref<number>(0);
+const newTask = ref<string>("");
+const tasks = ref<Task[]>([
+  { id: id.value++, title: "Faire le ménage", done: false}
+]);
+
+function addTask(): void {
+  tasks.value.push({ id: id.value++, title: newTask.value, done: false })
+  newTask.value = ""
+}
+
+function deleteTask(task: Task): void {
+  tasks.value = tasks.value.filter(t => t.id !== task.id)
+}
+
+const props = defineProps<Props>()
+</script>
+
+<template>
+  <h1>{{ title }}</h1>
+  <form @submit.prevent="addTask">
+    <input placeholder="New task ..." v-model="newTask" />
+    <button>Add task</button>
+  </form>
+  <ul>
+    <li v-for="task in tasks" :key="task.id">
+      <input type="checkbox" v-model="task.done"/>
+      {{ task.title }}
+      <button @click="deleteTask(task)">Delete</button>
+    </li>
+  </ul>
+</template>
+
+<style lang="css" scoped>
+</style>
